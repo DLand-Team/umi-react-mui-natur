0ceb8d08e0571284a391e7a57754153ac0128048
timothy <timothy.wang@zoom.us>
2022-12-03 15:48:22 +0800

    add tab size config

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,6 @@ module.exports = {
 
   rules: {
     // your rules
+    // "indent": ["error", 2]
   },
 };
--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -5,57 +5,58 @@ import { inject } from 'umi';
 const injector = inject('loading', 'toast', 'demo');
 
 const DemoPage = ({ loading, toast, demo }: typeof injector.type) => {
-	const showLoading = async () => {
-		loading.actions.show();
-		await sleep(3000);
-		loading.actions.hide();
-	};
-	const showToast = (type: 'error' | 'info' | 'success' | 'warning') => () => {
-		toast.actions[type](demo.state.toastText);
-	};
-	return (
-		<div>
-			<TextField
-				margin="normal"
-				label="loading text"
-				name="loading"
-				value={loading.state.loadingText}
-				onChange={(e) => loading.actions.changeLoadingText(e.target.value)}
-			/>
-			<br />
-			<Button variant="contained" color="info" onClick={showLoading}>
+  const showLoading = async () => {
+    loading.actions.show();
+    await sleep(3000);
+    loading.actions.hide();
+  };
+  const showToast = (type: 'error' | 'info' | 'success' | 'warning') => () => {
+    toast.actions[type](demo.state.toastText);
+  };
+
+  return (
+    <div>
+      <TextField
+        margin="normal"
+        label="loading text"
+        name="loading"
+        value={loading.state.loadingText}
+        onChange={(e) => loading.actions.changeLoadingText(e.target.value)}
+      />
+      <br />
+      <Button variant="contained" color="info" onClick={showLoading}>
 				show loading
-			</Button>
-			<br />
-			<TextField
-				margin="normal"
-				label="toast text"
-				name="toast"
-				value={demo.state.toastText}
-				onChange={(e) => demo.actions.updateToastText(e.target.value)}
-			/>
-			<br />
-			<Box mr={1} component='span'>
-				<Button variant="contained" color="error" onClick={showToast('error')}>
+      </Button>
+      <br />
+      <TextField
+        margin="normal"
+        label="toast text"
+        name="toast"
+        value={demo.state.toastText}
+        onChange={(e) => demo.actions.updateToastText(e.target.value)}
+      />
+      <br />
+      <Box mr={1} component="span">
+        <Button variant="contained" color="error" onClick={showToast('error')}>
 					show error toast
-				</Button>
-			</Box>
-			<Box mr={1} component='span'>
-				<Button variant="contained" color="info" onClick={showToast('info')}>
+        </Button>
+      </Box>
+      <Box mr={1} component="span">
+        <Button variant="contained" color="info" onClick={showToast('info')}>
 					show info toast
-				</Button>
-			</Box>
-			<Box mr={1} component='span'>
-				<Button variant="contained" color="success" onClick={showToast('success')}>
+        </Button>
+      </Box>
+      <Box mr={1} component="span">
+        <Button variant="contained" color="success" onClick={showToast('success')}>
 					show success toast
-				</Button>
-			</Box>
-			<Box mr={1} component='span'>
-				<Button variant="contained" color="warning" onClick={showToast('warning')}>
+        </Button>
+      </Box>
+      <Box mr={1} component="span">
+        <Button variant="contained" color="warning" onClick={showToast('warning')}>
 					show warning toast
-				</Button>
-			</Box>
-		</div>
-	);
+        </Button>
+      </Box>
+    </div>
+  );
 };
 export default injector(DemoPage);
