35106c6b63ee275291b9457ace2922951e50166e
timothy <timothy.wang@zoom.us>
2022-12-26 17:48:03 +0800

    add formItem component

--- src/components/FormItem/index.tsx
+++ src/components/FormItem/index.tsx
@@ -1,23 +1,81 @@
 import { Box } from '@mui/material';
-import { styled } from '@mui/system';
 import type { FieldAttributes } from 'formik';
 import { useField } from 'formik';
-import { ErrorMessage, Field } from 'formik';
+import { ErrorMessage } from 'formik';
+import { useEffect, useId, useRef, useState } from 'react';
+import { FormItemBox } from './style';
 
-export default function FormItem(props: FieldAttributes<any>) {
-	const [, field] = useField(props.name);
-	const errorMsg = field.touched ? field.error : '';
+export interface FormItemProps extends FieldAttributes<any> {
+	labelWidth?: number;
+	/**
+	 * form item layout
+	 */
+	layout?: 'vertical' | 'horizontal' | 'inline';
+	/**
+	 * show required sign(*)
+	 */
+	required?: boolean;
+}
+
+const dispalyMap = {
+	vertical: 'block',
+	horizontal: 'flex',
+	inline: 'inline-block',
+};
+
+const fieldDisplayMap = {
+	vertical: 'block',
+	horizontal: 'inline-block',
+	inline: 'inline-block',
+}
+
+export default function FormItem({
+	label,
+	as: Comp,
+	children,
+	name,
+	validate,
+	labelWidth,
+	required = false,
+	layout = 'horizontal',
+	...restProps
+}: FormItemProps) {
+	const [field, helper] = useField({ name, validate });
+	const errorMsg = helper.touched ? helper.error : '';
+	const fieldRef = useRef<HTMLDivElement>();
+	const [fieldHeight, setFieldHeight] = useState(40);
+
+	useEffect(() => {
+		if (fieldRef.current) {
+			const { height } = fieldRef.current.getBoundingClientRect();
+			if (height !== fieldHeight) {
+				setFieldHeight(height);
+			}
+		}
+	}, [fieldHeight]);
+	const id = useId();
 
 	return (
-		<div>
-			<Field {...props} error={!!errorMsg}>
-				{props.children}
-			</Field>
-			{errorMsg && (
-				<Box color="error.main" mt={1}>
-					{<ErrorMessage name={props.name} />}
+		<FormItemBox display={dispalyMap[layout]}>
+			{label && (
+				<Box className="label-box" component={'label'} for={id} width={labelWidth} display={dispalyMap[layout]}>
+					{required && <Box color='error.main'>* </Box>}{label}:
 				</Box>
 			)}
-		</div>
+			<Box display={fieldDisplayMap[layout]}>
+				<Comp {...restProps} {...field} id={id} error={!!errorMsg} ref={fieldRef}>
+					{children}
+				</Comp>
+
+				{errorMsg && (
+					<>
+						<br />
+						<Box color="error.main" pl={1} component={'span'}>
+							{<ErrorMessage name={name} />}
+						</Box>
+					</>
+				)}
+			</Box>
+		</FormItemBox>
 	);
 }
