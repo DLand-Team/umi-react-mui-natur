a323b7ee0201f0ce06ae2fa2defdba24b79f2a27
empty916 <empty916@qq.com>
2023-01-14 17:09:46 +0800

    add cdn

--- config/config.ts
+++ config/config.ts
@@ -1,28 +1,45 @@
-import routes from '../src/router'
+import routes from '../src/router';
 
 export default {
-  npmClient: 'npm',
-  mfsu: false,
-  plugins: ['umi-natur'],
-  natur: {
+	npmClient: 'npm',
+	mfsu: false,
+	plugins: ['umi-natur'],
+	natur: {
 		service: {},
 		persist: {
 			include: ['user'],
 			specific: {
 				user: 0,
-			}
+			},
 		},
 		useImmer: true,
-  },
-  styles: [
-    'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap',
-    'https://fonts.googleapis.com/icon?family=Material+Icons'
-  ],
-  metas: [
-    {
-      name: 'viewport',
-      content: 'initial-scale=1, width=device-width'
-    }
-  ],
-  routes
+	},
+	styles: [
+		'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap',
+		'https://fonts.googleapis.com/icon?family=Material+Icons',
+	],
+	externals: { react: 'React', 'react-dom': 'ReactDOM' },
+	headScripts: [
+		{
+			src: 'https://cdnjs.cloudflare.com/ajax/libs/react/18.2.0/umd/react.production.min.js',
+			integrity:
+				'sha512-8Q6Y9XnTbOE+JNvjBQwJ2H8S+UV4uA6hiRykhdtIyDYZ2TprdNmWOUaKdGzOhyr4dCyk287OejbPvwl7lrfqrQ==',
+			crossorigin: 'anonymous',
+			referrerpolicy: 'no-referrer',
+		},
+		{
+			src: 'https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.2.0/umd/react-dom.production.min.js',
+			integrity:
+				'sha512-MOCpqoRoisCTwJ8vQQiciZv0qcpROCidek3GTFS6KTk2+y7munJIlKCVkFCYY+p3ErYFXCjmFjnfTTRSC1OHWQ==',
+			crossorigin: 'anonymous',
+			referrerpolicy: 'no-referrer',
+		},
+	],
+	metas: [
+		{
+			name: 'viewport',
+			content: 'initial-scale=1, width=device-width',
+		},
+	],
+	routes,
 };
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -18,132 +18,126 @@ import { Message } from '@/utils/message';
 import { useLocation } from '@/utils/hooks';
 
 function Copyright(props: any) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+	return (
+		<Typography variant="body2" color="text.secondary" align="center" {...props}>
+			{'Copyright © '}
+			<Link color="inherit" href="https://mui.com/">
+				Your Website
+			</Link>{' '}
+			{new Date().getFullYear()}
+			{'.'}
+		</Typography>
+	);
 }
 
 const theme = createTheme();
 
-
 export default function SignInSide() {
-  const navigator = useNavigate();
+	const navigator = useNavigate();
 	React.useEffect(() => {
 		// 登陆页面重置store数据
-		store.globalResetStates()
-	}, [])
-	
-	const location = useLocation<{redirect?: string}>();
-	
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    const email = data.get('email') as string | null;
-    if (!email) {
+		store.globalResetStates();
+	}, []);
+
+	const location = useLocation<{ redirect?: string }>();
+
+	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		const data = new FormData(event.currentTarget);
+		const email = data.get('email') as string | null;
+		if (!email) {
 			Message.warning('Please Input Email!');
-    }
-    store.dispatch('loading', 'show');
-    await sleep(3000)
-    store.dispatch('user', 'updateName', email!);
-    store.dispatch('loading', 'hide');
+		}
+		store.dispatch('loading', 'show');
+		await sleep(3000);
+		store.dispatch('user', 'updateName', email!);
+		store.dispatch('loading', 'hide');
 		if (location.query.redirect) {
-			navigator(location.query.redirect)
+			navigator(location.query.redirect);
 		} else {
 			navigator('/');
 		}
-  };
+	};
 
-  return (
-    <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
-        <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random)',
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link href="#" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid>
-              <Copyright sx={{ mt: 5 }} />
-            </Box>
-          </Box>
-        </Grid>
-      </Grid>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<Grid container component="main" sx={{ height: '100vh' }}>
+				<CssBaseline />
+				<Grid
+					item
+					xs={false}
+					sm={4}
+					md={7}
+					sx={{
+						backgroundImage: 'url(https://source.unsplash.com/random)',
+						backgroundRepeat: 'no-repeat',
+						backgroundColor: (t) =>
+							t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+						backgroundSize: 'cover',
+						backgroundPosition: 'center',
+					}}
+				/>
+				<Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+					<Box
+						sx={{
+							my: 8,
+							mx: 4,
+							display: 'flex',
+							flexDirection: 'column',
+							alignItems: 'center',
+						}}
+					>
+						<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+							<LockOutlinedIcon />
+						</Avatar>
+						<Typography component="h1" variant="h5">
+							Sign in
+						</Typography>
+						<Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+							<TextField
+								margin="normal"
+								required
+								fullWidth
+								id="email"
+								label="Email Address"
+								name="email"
+								autoComplete="email"
+								autoFocus
+							/>
+							<TextField
+								margin="normal"
+								required
+								fullWidth
+								name="password"
+								label="Password"
+								type="password"
+								id="password"
+								autoComplete="current-password"
+							/>
+							<FormControlLabel
+								control={<Checkbox value="remember" color="primary" />}
+								label="Remember me"
+							/>
+							<Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+								Sign In
+							</Button>
+							<Grid container>
+								<Grid item xs>
+									<Link href="#" variant="body2">
+										Forgot password?
+									</Link>
+								</Grid>
+								<Grid item>
+									<Link href="#" variant="body2">
+										{"Don't have an account? Sign Up"}
+									</Link>
+								</Grid>
+							</Grid>
+							<Copyright sx={{ mt: 5 }} />
+						</Box>
+					</Box>
+				</Grid>
+			</Grid>
+		</ThemeProvider>
+	);
 }
