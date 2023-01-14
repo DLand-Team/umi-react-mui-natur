4cb24e88a835c336e387df34313824e4c693365a
empty916 <empty916@qq.com>
2023-01-14 20:47:26 +0800

    improve code

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -1,59 +1,61 @@
 import { isBrowser } from './index';
-import { useEffect, useRef } from "react"
-import { store, useLocation as useOriginLocation } from 'umi'
+import { useEffect, useRef } from 'react';
+import { store, useLocation as useOriginLocation } from 'umi';
 import qs from 'qs';
 import { useRequest } from 'ahooks';
-import { createUseInject, } from 'natur';
+import { createUseInject } from 'natur';
 
 /**
  * ahook的useMount在react strict模式下会执行两次，所以自己实现此方法
- * @param fn 
+ * @param fn
  */
 export const useMount = (fn: () => any) => {
-    const mountedRef = useRef(false);
-    useEffect(() => {
-        if (mountedRef.current) {
-            return;
-        }
-        mountedRef.current = true;
-        fn();
-    }, []);
-}
-
+	const mountedRef = useRef(false);
+	useEffect(() => {
+		if (mountedRef.current) {
+			return;
+		}
+		mountedRef.current = true;
+		fn();
+	}, []);
+};
 
 export const useBrowserInit = (fn: () => any) => {
-    const initRef = useRef(false);
-    if (!initRef.current && isBrowser) {
-        initRef.current = true;
-        fn();
-    }
+	const initRef = useRef(false);
+	if (!initRef.current && isBrowser) {
+		initRef.current = true;
+		fn();
+	}
+};
+
+export interface LocationQuery {
+	redirect?: string;
+	[k: string]: string | undefined;
 }
 
-export const useLocation = <T extends Record<string, string> = Record<string, string>>() => {
-    const $location = useOriginLocation();
-    let query: T = {};
-    if ($location.search) {
-        query = {
-            ...qs.parse($location.search.slice(1)),
-        }
-    }
-    return {
-        ...$location,
-        query
-    }
-}
+export const useLocation = <T extends LocationQuery = LocationQuery>() => {
+	const $location = useOriginLocation();
+	let query: T = {} as T;
+	if ($location.search) {
+		query = {
+			...qs.parse($location.search.slice(1)),
+		} as T;
+	}
+	return {
+		...$location,
+		query,
+	};
+};
 
-export type UseHttpArgs = Parameters<typeof useRequest>
-
+export type UseHttpArgs = Parameters<typeof useRequest>;
 
 /**
  * 包装一层useRequest
- * @param args 
- * @returns 
+ * @param args
+ * @returns
  */
 export const useHttp = (...args: UseHttpArgs) => {
- const res =	useRequest(...args);
- return res;
-}
+	return useRequest(...args);
+};
 
 export const useInject = createUseInject(() => store);
