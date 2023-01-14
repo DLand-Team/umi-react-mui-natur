5fef40ff79da83906edd6263e3e423651304f8a7
empty916 <empty916@qq.com>
2023-01-14 19:39:38 +0800

    improve code

--- src/layouts/loginLayout.tsx
+++ src/layouts/loginLayout.tsx
@@ -8,49 +8,58 @@ import { useEffect } from 'react';
 
 const formatRoutes = cloneDeep(routes);
 traverseObject(formatRoutes, (item) => {
-    if (Array.isArray(item?.routes)) {
-        item.children = item.routes;
-    }
+	if (Array.isArray(item?.routes)) {
+		item.children = item.routes;
+	}
 });
 traverseObject(formatRoutes, (item) => {
-    if (Array.isArray(item?.routes)) {
-        item.routes = undefined;
-    }
-})
-
+	if (Array.isArray(item?.routes)) {
+		item.routes = undefined;
+	}
+});
 
 function LoginLayout() {
-  const location = useLocation();
+	const location = useLocation();
 	const [user] = useInject('user');
-  const authList = matchRoutes(formatRoutes, location)?.map(i => (i.route as any)?.meta?.auth).filter(Boolean);
-  const navigator = useNavigate();
-	
-	
-  useEffect(() => {
-    if (!user.maps.isLogin) {
-      navigator('/login');
-    }
-  }, [navigator, user.maps.isLogin]);
-	
-  if (!authList?.every(user.maps.hasAuth)) {
-    return (
-        <div>
-            <h1>You do not have this permission</h1>
-            <Button variant='outlined' onClick={() => navigator({
-                pathname: `/`,
-            }, {
-                replace: true,
-            })}>Go Home</Button>
-        </div>
-    )
-  }
+	const authList = matchRoutes(formatRoutes, location)
+		?.map((i) => (i.route as any)?.meta?.auth)
+		.filter(Boolean);
+	const navigator = useNavigate();
 
+	useEffect(() => {
+		if (!user.maps.isLogin) {
+			navigator(`/login?redirect=${location.pathname + location.search}`);
+		}
+	}, [navigator, user.maps.isLogin, location.pathname, location.search]);
 
-  return (
-    <div>
-      <Outlet />
-    </div>
-  );
+	if (!authList?.every(user.maps.hasAuth)) {
+		return (
+			<div>
+				<h1>You do not have this permission</h1>
+				<Button
+					variant="outlined"
+					onClick={() =>
+						navigator(
+							{
+								pathname: `/`,
+							},
+							{
+								replace: true,
+							},
+						)
+					}
+				>
+					Go Home
+				</Button>
+			</div>
+		);
+	}
+
+	return (
+		<div>
+			<Outlet />
+		</div>
+	);
 }
 
 export default LoginLayout;
