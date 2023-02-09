5854bc67d8175642bf599889a17e10c7d17bc4c3
timothy <timothy.wang@zoom.us>
2023-02-09 13:36:23 +0800

    demo code dev

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -5,46 +5,62 @@ import TableBody from '@mui/material/TableBody';
 import MuiTable from '@mui/material/Table';
 import * as React from 'react';
 import type { ReactNode } from 'react';
+import { Box, TableContainer } from '@mui/material';
+import styles from './style.module.scss';
 
-export interface Column {
+export interface Column<D extends any = any> {
 	key: string;
 	title: string;
-	dataIndex?: string;
-	render?: (row: any) => ReactNode;
+	dataIndex?: any;
+	render?: (row: D, rowIndex: number, tableData: D[]) => ReactNode;
 }
+
+export type Columns<D extends any = any> = Column<D>[];
 
 export type Row = Record<string, any>;
 
 export interface TableProps<C extends Column = Column, R extends Row = Row> {
 	columns: C[];
 	rows: R[];
+	loading?: boolean;
 }
 
-function Table<C extends Column = Column, R extends Row = Row>({
+function Table<R extends Row = Row, C extends Column = Column>({
 	rows,
 	columns,
+	loading = false,
 }: TableProps<C, R>) {
 	return (
-		<MuiTable size="small">
-			<TableHead>
-				<TableRow>
-					{columns.map((c) => (
-						<TableCell key={c.key}>{c.title}</TableCell>
-					))}
-				</TableRow>
-			</TableHead>
-			<TableBody>
-				{rows.map((row, index) => (
-					<TableRow key={row.id || index}>
+		<TableContainer className={styles['table-container']}>
+			<MuiTable size="small">
+				<TableHead>
+					<TableRow>
 						{columns.map((c) => (
-							<TableCell key={c.key}>
-								{c.render ? c.render?.(row) : c.dataIndex ? row[c.dataIndex] : null}
-							</TableCell>
+							<TableCell key={c.key}>{c.title}</TableCell>
 						))}
 					</TableRow>
-				))}
-			</TableBody>
-		</MuiTable>
+				</TableHead>
+				<TableBody>
+					{rows.map((row, index) => (
+						<TableRow key={row.id || index}>
+							{columns.map((c) => (
+								<TableCell key={c.key}>
+									{c.render ? c.render?.(row, index, rows) : c.dataIndex ? row[c.dataIndex] : null}
+								</TableCell>
+							))}
+						</TableRow>
+					))}
+					{!rows.length && (
+						<TableRow>
+							<TableCell colSpan={columns.length}>
+								<Box className={styles['empty-table-box']}>EMPTY DATA</Box>
+							</TableCell>
+						</TableRow>
+					)}
+				</TableBody>
+			</MuiTable>
+			{loading && <Box className={styles['loading-box']}>Loading</Box>}
+		</TableContainer>
 	);
 }
 
--- src/pages/table.tsx
+++ src/pages/table.tsx
@@ -1,10 +1,14 @@
+import type { Columns } from '../components/Table';
 import { Table } from '../components/Table';
 import { Box, Button, TextField } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { fetchTableData } from '@/apis/demo';
 import { useHttp } from '@/utils/hooks';
+import type { PickPromiseType } from '@/utils/useAsyncFunction';
 
-const columns = [
+type Rows = PickPromiseType<typeof fetchTableData>;
+
+const columns: Columns<Rows[0]> = [
 	{
 		title: 'Name',
 		dataIndex: 'name',
@@ -23,7 +27,7 @@ const columns = [
 	{
 		title: 'Actions',
 		key: 'action',
-		render: (row: any) => {
+		render: (row) => {
 			return (
 				<Button
 					size={'small'}
@@ -38,22 +42,21 @@ const columns = [
 	},
 ];
 
-
 export default function TablePage() {
 	const [name, setName] = useState('');
-	const { data, loading, run } = useHttp(fetchTableData, {manual: true});
-	
+	const { data, loading, run } = useHttp(fetchTableData, { manual: true, debounceTime: 300 });
+
 	useEffect(() => {
 		run();
 	}, [run]);
-	
+
 	return (
 		<Box sx={{ p: 1 }}>
 			<div>
 				<TextField size={'small'} value={name} onChange={(e) => setName(e.target.value)} />
-				<Button disabled={loading} onClick={run}>Search</Button>
+				<Button onClick={run}>Search</Button>
 			</div>
-			<Table rows={data || []} columns={columns} />
+			<Table rows={data || []} columns={columns} loading={loading} />
 		</Box>
 	);
 }
