3d7fe00046129674ed9ae28094202dbdb6db0490
empty916 <empty916@qq.com>
2023-02-23 00:03:48 +0800

    improve ut

--- __tests__/greate-async/createAsyncController.ts
+++ __tests__/greate-async/createAsyncController.ts
@@ -1,7 +1,13 @@
-import { createAsyncController } from './../../src/utils/greate-async/asyncController';
+import type { AnyFn } from './../../src/utils/greate-async/common';
+import { cacheMap, createAsyncController } from './../../src/utils/greate-async/asyncController';
 import { sleep } from './../../src/utils/index';
 
-
+let fn: AnyFn | null = null;
+beforeEach(() => {
+	if (fn) {
+		cacheMap.get(fn)?.clear();
+	}
+})
 
 test('single', async () => {
 	let times = 0;
@@ -15,6 +21,7 @@ test('single', async () => {
 	}, {
 		single: true,
 	});
+	fn = getUserData;
 	const queuen = [];
 	for(let i = 0; i < 100; i++) {
 		// eslint-disable-next-line @typescript-eslint/no-loop-func
@@ -43,6 +50,7 @@ test('debounce time', async () => {
 	}, {
 		debounceTime: 90,
 	});
+	fn = getUserData;
 	const queuen = [];
 	for(let i = 0; i < 100; i++) {
 		// eslint-disable-next-line @typescript-eslint/no-loop-func
@@ -70,6 +78,7 @@ test('ttl', async () => {
 	}, {
 		ttl: 310,
 	});
+	fn = getUserData;
 	const resList = [];
 	for(let i = 0; i < 100; i++) {
 		resList.push(await getUserData(i % 3 + '' ));
@@ -97,7 +106,7 @@ test('ttl and debounce', async () => {
 		debounceTime: 50,
 		ttl: 160 * 4,
 	});
-
+	fn = getUserData;
 	await Promise.all([
 		getUserData(0 + '' ),
 		getUserData(1 + '' ),
@@ -118,6 +127,109 @@ test('ttl and debounce', async () => {
 	for(let i = 0; i < 100; i++) {
 		resList.push(await getUserData(i % 3 + '' ));
 	}
+	const uniquedReslist = [...new Set(resList)];
+	expect(uniquedReslist.length).toBe(3);
+	expect(uniquedReslist.find(i => i.name === '0')).not.toBe(null);
+	expect(uniquedReslist.find(i => i.name === '1')).not.toBe(null);
+	expect(uniquedReslist.find(i => i.name === '2')).not.toBe(null);
+	expect(times).toBe(3);
+});
+
+
+
+
+test('genKeyByParams', async () => {
+	let times = 0;
+	const getUserData = createAsyncController(async (name?: string) => {
+		times++;
+		await sleep(100);
+		return {
+			name: name || 'tom',
+			age: 10
+		}
+	}, {
+		ttl: 310,
+		genKeyByParams: ([name]) => name || '[]',
+	});
+	fn = getUserData;
+	const resList = [];
+	for(let i = 0; i < 100; i++) {
+		resList.push(await getUserData(i % 3 + '' ));
+	}
+	expect(cacheMap.get(getUserData)?.get('0')).not.toBe(null);
+	expect(cacheMap.get(getUserData)?.get('1')).not.toBe(null);
+	expect(cacheMap.get(getUserData)?.get('2')).not.toBe(null);
+
+	const uniquedReslist = [...new Set(resList)];
+	expect(uniquedReslist.length).toBe(3);
+	expect(uniquedReslist.find(i => i.name === '0')).not.toBe(null);
+	expect(uniquedReslist.find(i => i.name === '1')).not.toBe(null);
+	expect(uniquedReslist.find(i => i.name === '2')).not.toBe(null);
+	expect(times).toBe(3);
+});
+
+
+test('clear cache', async () => {
+	let times = 0;
+	const getUserData = createAsyncController(async (name?: string) => {
+		times++;
+		await sleep(100);
+		return {
+			name: name || 'tom',
+			age: 10
+		}
+	}, {
+		ttl: 310,
+		genKeyByParams: ([name]) => name || '[]',
+	});
+	fn = getUserData;
+	await getUserData('0');
+	expect(cacheMap.get(getUserData)?.get('0')).not.toBe(null);
+	
+	getUserData.clearCache('0');
+	expect(cacheMap.get(getUserData)?.get('0')).toBe(undefined);
+
+	const resList = [];
+	for(let i = 0; i < 100; i++) {
+		resList.push(await getUserData(i % 3 + '' ));
+	}
+	
+	expect(cacheMap.get(getUserData)?.get('0')).not.toBe(null);
+	expect(cacheMap.get(getUserData)?.get('1')).not.toBe(null);
+	expect(cacheMap.get(getUserData)?.get('2')).not.toBe(null);
+
+	const uniquedReslist = [...new Set(resList)];
+	expect(uniquedReslist.length).toBe(3);
+	expect(uniquedReslist.find(i => i.name === '0')).not.toBe(null);
+	expect(uniquedReslist.find(i => i.name === '1')).not.toBe(null);
+	expect(uniquedReslist.find(i => i.name === '2')).not.toBe(null);
+	expect(times).toBe(4);
+});
+
+
+test('clear all cache', async () => {
+	let times = 0;
+	const getUserData = createAsyncController(async (name?: string) => {
+		times++;
+		await sleep(100);
+		return {
+			name: name || 'tom',
+			age: 10
+		}
+	}, {
+		ttl: 310,
+		genKeyByParams: ([name]) => name || '[]',
+	});
+	fn = getUserData;
+	const resList = [];
+	for(let i = 0; i < 100; i++) {
+		resList.push(await getUserData(i % 3 + '' ));
+	}
+	getUserData.clearCache();
+	expect(cacheMap.get(getUserData)?.get('0')).toBe(undefined);
+	expect(cacheMap.get(getUserData)?.get('1')).toBe(undefined);
+	expect(cacheMap.get(getUserData)?.get('2')).toBe(undefined);
+
 	const uniquedReslist = [...new Set(resList)];
 	expect(uniquedReslist.length).toBe(3);
 	expect(uniquedReslist.find(i => i.name === '0')).not.toBe(null);
