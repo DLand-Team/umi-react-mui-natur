0153c042cb6b228c9f65dde4157a4e0a8d221826
empty916 <empty916@qq.com>
2023-06-10 11:11:56 +0800

    improve code render component

--- src/components/CodeRender/style.tsx
+++ src/components/CodeRender/style.tsx
@@ -2,7 +2,34 @@ import { styled } from '@mui/material';
 
 export const CodeStyle = styled('code')``;
 
+
 export const Pre = styled('pre')`
+	table, tbody, tr, td {
+		background-color: inherit;
+	}
+	.hljs-ln {
+		border-collapse: collapse;
+	}
+
+	.hljs-ln td {
+		
+	}
+	.hljs-ln-n:before {
+		content: attr(data-line-number);
+		color: rgb(101, 103, 108);
+	}
+	.hljs-ln-numbers {
+		position: sticky;
+		left: 0;
+		box-sizing: border-box;
+	}
+	.hljs-ln-n {
+		padding:0 10px 0 20px;
+		border-right: 1px solid rgb(16, 17, 19);
+	}
+	.hljs-ln-code {
+		padding-left: 10px;
+	}
 	code {
 		margin: 0;
 		border-radius: 6px;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,26 @@
+import CodeRender from "@/components/CodeRender";
 import Dashboard from "./dashboard/Dashboard";
+import { Box } from "@mui/material";
 
 
 
 const Home = () => {
   return (
-    <>
-			<Dashboard />
-		</>
+    <Box p={4}>
+			<CodeRender
+				showLineNumber
+				language="json"
+				code={JSON.stringify({
+					a: 1,
+					b: 2,
+					c: {
+						c1: 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+						c2: [1, 2,3]
+					}
+				}, null, 2)}
+			/>
+			{/* <Dashboard /> */}
+		</Box>
   );
 };
 
